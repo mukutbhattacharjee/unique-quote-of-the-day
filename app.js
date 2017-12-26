af744e3dd7cce66b261433a20745454f75fcca4e
Mukut <M1037588@mindtree.com>
2017-12-26 19:41:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express();
+
+var port = 4000;
+
+app.get("/",(req,res)=>{
+    var random = Math.floor(Math. random() * 6) + 1
+    res.send(quotes[random])
+})
+
+var httpServer = app.listen(port,()=>{
+    console.log("listenning at port "+port);
+})
+
+var quotes = [
+    "I love you the more in that I believe you had liked me for my own sake and for nothing else.",
+    "But man is not made for defeat. A man can be destroyed but not defeated.",
+    "When you reach the end of your rope, tie a knot in it and hang on.",
+    "There is nothing permanent except change.",
+    "You cannot shake hands with a clenched fist.",
+    "The most difficult thing is the decision to act, the rest is merely tenacity. The fears are paper tigers. You can do anything you decide to do. You can act to change and control your life; and the procedure, the process is its own reward.",
+    "There is no charm equal to tenderness of heart."
+]
+
+
